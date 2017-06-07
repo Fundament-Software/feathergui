3fefe2999a7c5d839463807fb813a54c93c58a66
Erik McClure <erikm@blackspherestudios.com>
2017-06-06 23:30:47 -0700

    Add pack.bat for generating windows SDK

--- bindings/fgRust/feathergui-sys/gen_bindings.ps1
+++ bindings/fgRust/feathergui-sys/gen_bindings.ps1
@@ -1,3 +1,2 @@
 bindgen ..\..\..\include\fgAll.h -o src/lib.rs --whitelist-var "[^_].*" --whitelist-type "
-.*" --whitelist-function "[^_].*" --raw-line "#![feature(untagged_unions)]" --raw-line "#![allow(non_camel_case_types, non_snake_case)]" -- -x c++ -std=c++1
-4 -D_RUST_BINDGEN
+.*" --whitelist-function "[^_].*" --raw-line "#![feature(untagged_unions)]" --raw-line "#![allow(non_camel_case_types, non_snake_case)]" -- -x c++ -std=c++14 -D_RUST_BINDGEN
