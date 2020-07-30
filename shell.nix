8cf264e22f72ef68312ee1812b082b9b02e8015b
Alex Iverson <alexjiverson@gmail.com>
2020-07-30 01:06:00 -0500

    made submodule location change work in shell.nix too

--- shell.nix
+++ shell.nix
@@ -2,4 +2,8 @@
 
 with pkgs;
 with callPackage ./lib.nix { };
-terraShell { buildInputs = [ pkgs.libiconv pkgs.glibc.dev ]; }
+terraShell {
+  buildInputs = [ pkgs.libiconv pkgs.glibc.dev ];
+  TERRA_PATH = "./?.t;deps/?.t";
+  LUA_PATH = "./?.lua;deps/?.lua";
+}
